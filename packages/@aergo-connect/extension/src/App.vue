01a589098406e1a24ebbaff7ec26dbe1c249bd67
Paul Grau <paul@blocko.io>
2020-03-04 15:38:33 +0900

    Cleanup and prepare extension

---
+++ packages/@aergo-connect/extension/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    App
+    
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  background-color: #f0f0f0;
+}
+
+#app {
+  max-width: 360px;
+  margin: 0 auto;
+  background-color: #fff;
+}
+
+</style>
---
+++ packages/@aergo-connect/extension/src/views/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    <h1 class="big-title">Welcome to Aergo Connect</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+});
+</script>
+
+<style lang="scss">
+.big-title {
+  font-size: (44/16)*1rem;
+}
